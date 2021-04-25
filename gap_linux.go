76d0345a7b6631d8fa7d8355763ee51ce84a5492
Raphael Baron <rbaron@rbaron.net>
2021-04-25 11:14:18 +0200

    Add support for advertisement service data on linux
    Tested on my raspberry pi running raspberry pi os with bluez + dbus API

--- gap_linux.go
+++ gap_linux.go
@@ -182,6 +182,10 @@ func (a *Adapter) Scan(callback func(*Adapter, ScanResult)) error {
 						props.Name = val.Value().(string)
 					case "UUIDs":
 						props.UUIDs = val.Value().([]string)
+					case "ServiceData":
+						if converted, ok := val.Value().(map[string]interface{}); ok {
+							props.ServiceData = converted
+						}
 					}
 				}
 				callback(a, makeScanResult(props))
@@ -222,6 +226,19 @@ func makeScanResult(props *device.Device1Properties) ScanResult {
 	a := Address{MACAddress{MAC: addr}}
 	a.SetRandom(props.AddressType == "random")
 
+	var serviceDatas []AdvServiceData
+	for k, val := range props.ServiceData {
+		parsedUUID, err := ParseUUID(k)
+		if err != nil || val == nil {
+			continue
+		}
+		variant := val.(dbus.Variant)
+		serviceDatas = append(serviceDatas, AdvServiceData{
+			UUID: parsedUUID,
+			Data: variant.Value().([]byte),
+		})
+	}
+
 	return ScanResult{
 		RSSI:    props.RSSI,
 		Address: a,
@@ -229,6 +246,7 @@ func makeScanResult(props *device.Device1Properties) ScanResult {
 			AdvertisementFields{
 				LocalName:    props.Name,
 				ServiceUUIDs: serviceUUIDs,
+				ServiceDatas: serviceDatas,
 			},
 		},
 	}
